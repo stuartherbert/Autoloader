15399b1ed81d00a3e9966ceef5cf3a4507c2f9e5
Stuart Herbert <stuart@stuartherbert.com>
2011-08-02 23:03:21 +0100

    Added new searchFirst() and searchLast() methods

--- src/php/psr0.autoloader.php
+++ src/php/psr0.autoloader.php
@@ -1,7 +1,7 @@
 <?php
 
 // autoloader support
-function __autoloader_normalise_path($className)
+function psr0_autoloader_normalise_path($className)
 {
 	 $fileName  = '';
 	 $lastNsPos = strripos($className, '\\');
@@ -17,7 +17,7 @@ function __autoloader_normalise_path($className)
 
 }
 
-function __autoloader_init_namespace($namespace, $fileExt = '.init.php')
+function psr0_autoloader_init_namespace($namespace, $fileExt = '.init.php')
 {
         static $loadedNamespaces = array();
 
@@ -28,19 +28,14 @@ function __autoloader_init_namespace($namespace, $fileExt = '.init.php')
                 return;
         }
 
-        $path = __autoloader_normalise_path($namespace);
+        $path = psr0_autoloader_normalise_path($namespace);
         $filename = $path . '/_init/' . end(explode('/', $path)) . $fileExt;
         $loadedModules[$namespace] = $filename;
 
-        __autoloader_include($filename);
+        psr0_autoloader_include($filename);
 }
 
-function __autoloader_init_tests($namespace)
-{
-        __autoloader_namespace($namespace, '.initTests.php');
-}
-
-function __autoloader_autoload($classname)
+function psr0_autoloader_autoload($classname)
 {
         if (class_exists($classname) || interface_exists($classname))
         {
@@ -48,12 +43,12 @@ function __autoloader_autoload($classname)
         }
 
         // convert the classname into a filename on disk
-        $classFile = __autoloader_normalise_path($classname) . '.php';
+        $classFile = psr0_autoloader_normalise_path($classname) . '.php';
 
-        return __autoloader_include($classFile);
+        return psr0_autoloader_include($classFile);
 }
 
-function __autoloader_include($filename)
+function psr0_autoloader_include($filename)
 {
        	$pathToSearch = explode(PATH_SEPARATOR, get_include_path());
 
@@ -81,31 +76,52 @@ function __autoloader_include($filename)
         return FALSE;
 }
 
-function __autoloader_alsoSearch($dir)
+function psr0_autoloader_searchFirst($dir)
 {
-        $dir = realpath($dir);
+	$dir = realpath($dir);
 
+	psr0_autoloader_dontSearchIn($dir);
+
+        // add the new directory to the front of the path
+        set_include_path($dir . PATH_SEPARATOR . get_include_path());
+}
+
+function psr0_autoloader_searchLast($dir)
+{
+	$dir = realpath($dir);
+
+	psr0_autoloader_dontSearchIn($dir);
+
+        // add the new directory to the end of the path
+        set_include_path(get_include_path() . PATH_SEPARATOR . $dir);
+}
+
+function psr0_autoloader_dontSearchIn($dir)
+{
         // check to make sure that $dir is not already in the path
-        $pathToSearch = explode(PATH_SEPARATOR, get_include_path());
+	$pathToSearch = explode(PATH_SEPARATOR, get_include_path());
 
         foreach ($pathToSearch as $dirToSearch)
         {
                 $dirToSearch = realpath($dirToSearch);
                 if ($dirToSearch == $dir)
-                {
-                        // we have found it
-                        // remove it from the list
-                        $key = key($pathToSearch);
-                        unset($pathToSearch[$key]);
+		{
+			// we have found it
+			// remove it from the list
+			// $key points to the *next* entry in the list,
+			// not the current entry
+			$key = key($pathToSearch);
+			$key -= 1;
+			unset($pathToSearch[$key]);
                 }
         }
 
-        // add the new directory to the front of the path
-        set_include_path($dir . PATH_SEPARATOR . implode(PATH_SEPARATOR, $pathToSearch));
+        // set the revised search path
+        set_include_path(implode(PATH_SEPARATOR, $pathToSearch));
 }
 
-spl_autoload_register('__autoloader_autoload');
+spl_autoload_register('psr0_autoloader_autoload');
 // assume that we are at the top of a vendor tree to load from
-__autoloader_alsoSearch(__DIR__);
+psr0_autoloader_searchFirst(__DIR__);
 
 ?>
