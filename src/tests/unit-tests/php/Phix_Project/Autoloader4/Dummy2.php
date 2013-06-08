3f914f0015f6ce3a6c3c724938a872450facdaa7
Stuart Herbert <stuart@stuartherbert.com>
2013-06-08 16:32:26 +0100

    New: workaround for PHP 5.3 fatal error when testing

--- src/tests/unit-tests/php/Phix_Project/Autoloader4/Dummy2.php
+++ src/tests/unit-tests/php/Phix_Project/Autoloader4/Dummy2.php
@@ -2,7 +2,9 @@
 
 namespace Phix_Project\Autoloader4;
 
-class Dummy2
-{
-        use Trait1;
+if (PHP_MAJOR_VERSION > 5 || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 4)) {
+	class Dummy2
+	{
+	        use Trait1;
+	}
 }
